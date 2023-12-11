f389985f6f16275b61ec341cb6a0fb216facf1da
Jacob Yealy <jacobyealy@gmail.com>
2023-12-10 19:04:15 -0500

    Teacher preference model constructed. Testing needed.

--- src/main.py
+++ src/main.py
@@ -1,15 +1,29 @@
 from genetic_algorithm import GeneticAlgorithm
 from teacher import Teacher
 from schedule import Schedule
+import pandas as pd
+
+def load_teachers_from_excel(file_path):
+    df = pd.read_excel(file_path)
+    teachers = []
+    for index, row in df.iterrows():
+        # Assuming each row in the DataFrame corresponds to a teacher and the columns match the teacher's attributes
+        teachers.append(Teacher(
+            id=row['Teacher ID'],
+            min_sections=row['Min Sections'],
+            max_sections=row['Max Sections'],
+            board_pref=row['Board Pref'],
+            time_pref=row['Time Pref'],
+            days_pref=row['Days of Week'],
+            section_prefs={i: row[i] for i in range(1, 30)}  # Adjust the range based on your data
+        ))
+    return teachers
 
 def main():
-    # Initialize teachers with example data
-    teachers = [
-        Teacher(id=1, min_sections=2, max_sections=4, board_pref=1, time_pref=1, days_pref=2, section_prefs=[...]),  # Replace with actual preferences
-        # Add more Teacher instances
-    ]
+    teachers = load_teachers_from_excel('teachers_preferences.xlsx')  # Use your actual file path
 
     # Initialize other parameters with example data
+    # These would also be loaded from your data files or defined based on your problem's needs
     course_sections = [...]  # Replace with actual course section identifiers
     classrooms = [...]  # Replace with actual classroom identifiers
     time_modules = [...]  # Replace with actual time module identifiers
